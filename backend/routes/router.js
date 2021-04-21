83d56d68cf135aaabcd534bdb634dcc0f1ad2990
Arahabica <r.sasage@gmail.com>
2021-04-21 12:42:13 +0900

    delete v2 API

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,7 +1,6 @@
 const AWS = require('aws-sdk')
 const axios = require('axios')
 const express = require('express')
-const Crypto = require("crypto")
 const app = express.Router()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -10,8 +9,7 @@ const LINE_CHANNEL_ID = process.env.LINE_CHANNEL_ID
 
 const TableName = {
   User: process.env.USER_TABLE,
-  Visit: process.env.VISIT_TABLE,
-  Token: process.env.TOKEN_TABLE
+  Visit: process.env.VISIT_TABLE
 }
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -96,28 +94,6 @@ const getVisitList = async () => {
   return data.Items;
 }
 
-const putToken = async (data) => {
-  const params = {
-    TableName: TableName.Token,
-    Item: {
-      token: data.token,
-      userId: data.userId,
-      expiredAt: data.expiredAt,
-      updatedAt: data.updatedAt
-    }
-  };
-  return await dynamodb.put(params).promise();
-}
-
-const getTokenInfo = async (token) => {
-  const params = {
-    TableName: TableName.Token,
-    Key: { token }
-  }
-  const res = await dynamodb.get(params).promise()
-  return res.Item
-}
-
 app.put('/v1/user', async function(req, res) {
   try {
     const { accessToken } = req.body
@@ -174,59 +150,4 @@ app.get('/v1/visit', async function(req, res) {
   }
 });
 
-app.post('/v2/token', async function(req, res) {
-  try {
-    const { accessToken } = req.body
-    const now = new Date().getTime()
-    // LINEのアクセストークンが正しいか検証
-    await verifyToken(accessToken)
-    // アクセストークンを利用してプロフィール取得
-    const profile = await getProfile(accessToken)
-    const userId = profile.userId
-
-    // ランダムな文字列を生成
-    const token  = Crypto.randomBytes(16).toString("hex")
-    await putToken({
-      token,
-      userId,
-      expiredAt: now + 20 * 1000, // 20秒だけ有効
-      updatedAt: now,
-    })
-    res.json({success: 'succeed!', url: req.url, data: { token }})
-  } catch (err) {
-    console.error(err)
-    res.statusCode = 500;
-    res.json({error: err, url: req.url, body: req.body});
-  }
-});
-
-app.post('/v2/qrCode', async function(req, res) {
-  try {
-    console.log('/v2/qrCode')
-    const { qrCode } = req.body
-    const token = qrCode
-    const visitedAt = new Date().getTime()
-    const tokenInfo = await getTokenInfo(token)
-    if (!tokenInfo) {
-      res.statusCode = 401;
-      return res.json({error: 'Token is not found.', url: req.url, body: req.body});
-    } else if (visitedAt > tokenInfo.expiredAt) {
-      res.statusCode = 401;
-      return res.json({error: 'Token is expired.', url: req.url, body: req.body});
-    }
-    const userId = tokenInfo.userId
-    const user = await getUser(userId)
-    await putVisit( {
-      userId,
-      visitedAt: visitedAt,
-      displayName: user.displayName,
-      pictureUrl: user.pictureUrl,
-    });
-    res.json({success: 'succeed!', url: req.url, data: {}});
-  } catch (err) {
-    console.error(err)
-    res.statusCode = 500;
-    res.json({error: err, url: req.url, body: req.body});
-  }
-});
 module.exports = app
